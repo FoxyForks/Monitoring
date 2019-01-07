76db7471fdda1804ca3478c0aa593d004dac6d3c
Patrik Kristian <patrik@pkristian.cz>
2019-01-08 00:34:33 +0100

    ENV variables for docker

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+##paste env variables
+echo "parameters:" \
+> ./app/config/env.neon
+
+env \
+| grep ^APP_ \
+| sort \
+| sed 's/=/: "/' \
+| sed 's/$/"/' \
+| sed 's/^/\t/' \
+>> ./app/config/env.neon
+
+
 [[ -d vendor ]] || composer install
 php www/index.php migrations:continue
 php www/index.php rabbitmq:setup-fabric
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -18,7 +18,13 @@ $configurator->setTimeZone('Europe/Prague');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+if (file_exists(__DIR__ . '/config/env.neon')) {
+    $configurator->addConfig(__DIR__ . '/config/docker.neon');
+    $configurator->addConfig(__DIR__ . '/config/env.neon');
+} else {
+    $configurator->addConfig(__DIR__ . '/config/config.local.neon');
+}
 
 $container = $configurator->createContainer();
 
